e1799a8624b41a7cb7ce4704668b937e93d0c02c
Joel <joelthebeast50@gmail.com>
2023-12-28 23:18:40 -0800

    define Monster class

---
+++ Monsters.py
@@ -0,0 +1,9 @@
+class Monsters:
+    def __init__ (self, name: str, species: str, strength: int, health: int, intelligence: int):
+        self.name = name
+        self.species = species 
+        self.strength = strength 
+        self.health = health
+        self.intelligence = intelligence 
+
+
